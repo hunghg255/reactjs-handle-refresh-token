2287c148480805a6fc95cc9334af1b4fe3c52e61
hunghg255 <giahunghust@gmail.com>
2023-09-21 18:03:00 +0700

    chore: update

--- src/apis/apollo-client/apollo-client.ts
+++ src/apis/apollo-client/apollo-client.ts
@@ -1,8 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-loop-func */
-import { ApolloClient, ApolloLink, fromPromise, InMemoryCache, gql } from '@apollo/client';
+// @ts-nocheck
+import { ApolloClient, ApolloLink, fromPromise, InMemoryCache, split } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { createUploadLink } from 'apollo-upload-client';
 import { createClient } from 'graphql-ws';
 import Cookies from 'js-cookie';
@@ -85,64 +88,73 @@ const resolvePendingRequests = (newToken) => {
   pendingRequests = [];
 };
 
-const errorLink = onError(({ graphQLErrors, networkError, operation, forward }) => {
+const errorLink = onError(({ graphQLErrors, operation, forward }) => {
   if (graphQLErrors) {
     for (const err of graphQLErrors) {
-      switch (err.extensions.code) {
-        case 'UNAUTHENTICATED':
-          if (operation.operationName === 'refreshUserToken') return;
-
-          let forward$;
-
-          if (!isRefreshing) {
-            isRefreshing = true;
-            forward$ = fromPromise(
-              onRefreshToken()
-                .then((accessToken) => {
-                  // Store the new tokens for your auth link
-                  resolvePendingRequests(accessToken);
-                  return accessToken;
-                })
-                .catch(() => {
-                  pendingRequests = [];
-                  // Handle token refresh errors e.g clear stored tokens, redirect to login, ...
-                  return;
-                })
-                .finally(() => {
-                  isRefreshing = false;
-                }),
-            ).filter((value) => Boolean(value));
-          } else {
-            // Will only emit once the Promise is resolved
-            forward$ = fromPromise(
-              new Promise((resolve) => {
-                pendingRequests.push((newToken: string) => resolve(newToken));
-              }),
-            );
-          }
-
-          return forward$.flatMap((newToken) => {
-            if (newToken) {
-              const oldHeaders = operation.getContext().headers;
-              // modify the operation context with a new token
-              operation.setContext({
-                headers: {
-                  ...oldHeaders,
-                  authorization: `Bearer ${newToken}`,
-                },
-              });
-            }
-
-            return forward(operation);
-          });
+      // Pass through if the error is not an authentication error
+      if ((err as any).extensions?.response?.message !== 'Unauthorized') {
+        forward(operation);
+        continue;
       }
+
+      if (operation.operationName === 'refreshUserToken') return;
+
+      let forward$;
+
+      if (!isRefreshing) {
+        isRefreshing = true;
+        forward$ = fromPromise(
+          onRefreshToken()
+            .then((accessToken) => {
+              // Store the new tokens for your auth link
+              resolvePendingRequests(accessToken);
+              return accessToken;
+            })
+            .catch(() => {
+              pendingRequests = [];
+              // Handle token refresh errors e.g clear stored tokens, redirect to login, ...
+              return;
+            })
+            .finally(() => {
+              isRefreshing = false;
+            }),
+        ).filter((value) => Boolean(value));
+      } else {
+        // Will only emit once the Promise is resolved
+        forward$ = fromPromise(
+          new Promise((resolve) => {
+            pendingRequests.push((newToken: string) => resolve(newToken));
+          }),
+        );
+      }
+
+      return forward$.flatMap((newToken) => {
+        if (newToken) {
+          const oldHeaders = operation.getContext().headers;
+          // modify the operation context with a new token
+          operation.setContext({
+            headers: {
+              ...oldHeaders,
+              authorization: `Bearer ${newToken}`,
+            },
+          });
+        }
+
+        return forward(operation);
+      });
     }
   }
-
-  if (networkError) {
-    console.log(`[Network error]: ${networkError}`);
-  }
 });
+
+// const splitLink = split(
+//   ({ query }) => {
+//     const definition = getMainDefinition(query);
+
+//     return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+//   },
+//   wsLink,
+//   ApolloLink.from([errorLink, authLink, uploadLink]),
+// );
 
 export const client = new ApolloClient({
   link: ApolloLink.from([authLink, errorLink, uploadLink, wsLink]),
